759b10666b3e5a51758889a0ca077c607ae85083
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-03-12 09:01:45 +0000

    [update]修正とフォロー

---
+++ db/migrate/20240312085343_create_relatonships.rb
@@ -0,0 +1,11 @@
+class CreateRelatonships < ActiveRecord::Migration[6.1]
+  def change
+    create_table :relatonships do |t|
+
+      t.integer :follower_id
+      t.integer :followed_id
+
+      t.timestamps
+    end
+  end
+end
