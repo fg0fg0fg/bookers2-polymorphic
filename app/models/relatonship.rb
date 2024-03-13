43e94cdb015fdd0694aa2c3fb31af8b3f1c28495
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-03-13 04:15:09 +0000

    [update]follow

--- app/models/relatonship.rb
+++ app/models/relatonship.rb
@@ -1,6 +1,8 @@
 class Relatonship < ApplicationRecord
 
 belongs_to :follower, class_name: "User"
-belongs_to :followed, class_name: "U"
-
+belongs_to :followed, class_name: "User"
+#fo架空のテーブルに所属、Usクラスを参照
+#フォローする人
+#フォローされる人
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,15 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many :relationships, class_name: "Relationship", foreigen_key: "follower_id", dependent: :destroy
-  has_many :relationships, class_name: "Relationship", foreigen_key: "followed_id", dependent: :destroy
-  has_many :followers, through: :relationships, source: :follower
-  has_many :followeds, through: :relationships, source: :followed
+  has_many :active_relationships, class_name: "Relationship", foreigen_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreigen_key: "followed_id", dependent: :destroy
+  #複数のfo架空のテーブルを所持、Reクラスを参照、外部キーfoカラム
+  #フォローしている人
+  #フォローされている人
+  has_many :following_users, through: :active_relationships, source: :followed
+  has_many :follower_users, through: :passive_relationships, source: :follower
+  #自分がフォローしている人
+  #自分をフォローしている人
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -19,5 +24,17 @@ class User < ApplicationRecord
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  def follow(user_id)
+    active_relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    active_relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    following_users.include?(user)
   end
 end
