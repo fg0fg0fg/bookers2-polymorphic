d2100f403d85b5332bd30cc067a4c4b72c736898
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-03-20 09:33:32 +0000

    [update]非同期

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,12 @@
 class SearchesController < ApplicationController
   def search
-    @range = params[:range]
-    @word = params[:word]
-    if @range == "User"
+    @range = params[:range]#検索モデル
+    @word = params[:word]#検索ワード
+    if @range == "User"#UserかBook
       @users = User.looks(params[:search], @word)
     else
       @books = Book.looks(params[:search], @word)
     end
   end
 end
+#params[:search]検索方法
